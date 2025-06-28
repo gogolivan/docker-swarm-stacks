74bd76524f2f6ee4495a10257126ec876c21ce78
Ivan Gogol <>
2025-06-28 15:50:31 +0200

    refactor: grafana services

--- variables.tf
+++ variables.tf
@@ -21,9 +21,6 @@ variable "stack_service_replicas_env_config" {
     LOCALSTACK_REPLICAS = 1
     PROMETHEUS_REPLICAS = 1
     GRAFANA_REPLICAS    = 1
-    LOKI_REPLICAS       = 1
-    TEMPO_REPLICAS      = 1
-    PROMTAIL_REPLICAS   = 1
   }
 }
 
